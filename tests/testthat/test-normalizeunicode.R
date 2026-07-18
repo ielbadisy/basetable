bee0efe5e123976e798ea4c6611bbdb7b026f201
ielbadisy <elbadisyimad@gmail.com>
2026-07-18 14:23:37 +0100

    normalizeunicode

---
+++ tests/testthat/test-normalizeunicode.R
@@ -0,0 +1,7 @@
+test_that("normalizeunicode normalizes Unicode text", {
+  testthat::skip_if_not_installed("stringi")
+
+  x <- "e\u0301"
+
+  expect_equal(normalizeunicode(x, form = "NFC"), "é")
+})
