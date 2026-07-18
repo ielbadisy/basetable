a7b0035313ac2a3fae8c6c6341eea953303373c3
ielbadisy <elbadisyimad@gmail.com>
2026-07-18 17:30:33 +0100

    Dispatch split() to data.table's native split.data.table() instead of interaction()+base::split()

--- R/split.R
+++ R/split.R
@@ -1,16 +1,8 @@
 split <- function(data, by, drop = FALSE, keep.by = FALSE) {
-  df <- bt_as_data_frame(data)
-  by <- bt_resolve_cols(df, by)
-  key <- interaction(df[, by, drop = FALSE], drop = drop, lex.order = TRUE)
-  out <- base::split(df, key, drop = drop)
-
-  if (!keep.by) {
-    out <- lapply(out, function(piece) bt_as_tibble(piece[, setdiff(names(piece), by), drop = FALSE]))
-  } else {
-    out <- lapply(out, bt_as_tibble)
-  }
-
-  out
+  dt <- bt_as_data_table_ro(data)
+  by <- bt_resolve_cols(dt, by)
+  out <- base::split(dt, by = by, drop = drop, keep.by = keep.by, sorted = TRUE)
+  lapply(out, bt_as_tibble)
 }
 
 by_apply <- function(data, by, fun, ..., bind = FALSE, id = ".group") {
