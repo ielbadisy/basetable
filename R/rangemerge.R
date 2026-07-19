4c42ee2063acbd9af52cf2a12179f73b901f7e0d
ielbadisy <elbadisyimad@gmail.com>
2026-07-19 17:48:56 +0100

    Implement real range-join filtering in rangemerge() via foverlaps() (lower/upper were validated but never applied)

--- R/rangemerge.R
+++ R/rangemerge.R
@@ -1,10 +1,11 @@
-rangemerge <- function(x, y, by, lower, upper) {
+rangemerge <- function(x, y, by, lower, upper, value) {
   x_dt <- bt_as_data_table_ro(x)
   y_dt <- bt_as_data_table_ro(y)
   by <- bt_resolve_cols(x_dt, by)
   bt_resolve_cols(y_dt, by)
   lower <- bt_resolve_cols(x_dt, lower)
   upper <- bt_resolve_cols(x_dt, upper)
+  value <- bt_resolve_cols(y_dt, value)
 
   if (length(by) < 1L) {
     stop("`by` must contain at least one column.", call. = FALSE)
@@ -12,6 +13,24 @@ rangemerge <- function(x, y, by, lower, upper) {
   if (length(lower) != 1L || length(upper) != 1L) {
     stop("`lower` and `upper` must each name one column.", call. = FALSE)
   }
+  if (length(value) != 1L) {
+    stop("`value` must name one column.", call. = FALSE)
+  }
 
-  data.table::merge.data.table(x_dt, y_dt, by = by, all.x = TRUE, sort = FALSE)
+  x_tmp <- data.table::copy(x_dt)
+  y_tmp <- data.table::copy(y_dt)
+  value_end <- "__bt_value_end"
+  y_tmp[[value_end]] <- y_tmp[[value]]
+  data.table::setkeyv(y_tmp, c(by, value, value_end))
+
+  out <- data.table::foverlaps(
+    x_tmp,
+    y_tmp,
+    by.x = c(by, lower, upper),
+    by.y = data.table::key(y_tmp),
+    type = "any",
+    nomatch = NA
+  )
+  out[[value_end]] <- NULL
+  out[]
 }
