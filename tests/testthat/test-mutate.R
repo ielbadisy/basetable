49de3459055ae84e89ab07e92ffdd745f505a806
Imad EL BADISY <elbadisyimad@gmail.com>
2026-08-24 11:35:49 +0100

    Test transform caller-scope semantics (#97)

--- tests/testthat/test-mutate.R
+++ tests/testthat/test-mutate.R
@@ -5,3 +5,30 @@ test_that("mutate adds derived columns", {
   expect_true("ratio" %in% names(out))
   expect_equal(out$ratio[[1]], mtcars$mpg[[1]] / mtcars$cyl[[1]])
 })
+
+test_that("transform verbs resolve values from their caller", {
+  data <- data.frame(value = 1:3)
+
+  run_transform <- function(x) {
+    multiplier <- 3
+    transform(x, result = value * multiplier)
+  }
+  run_mutate <- function(x) {
+    offset <- 2
+    mutate(x, result = value + offset)
+  }
+  run_transmute <- function(x) {
+    denominator <- 2
+    transmute(x, result = value / denominator)
+  }
+
+  expect_equal(run_transform(data)$result, c(3, 6, 9))
+  expect_equal(run_mutate(data)$result, c(3, 4, 5))
+  expect_equal(run_transmute(data)$result, c(0.5, 1, 1.5))
+})
+
+test_that("transform does not confuse an evaluated result with a value column", {
+  data <- data.frame(value = 1:3)
+
+  expect_equal(transform(data, doubled = value * 2)$doubled, c(2, 4, 6))
+})
