3430ae4bf07fdaada363d20a2d644d5187f6b4dd
ielbadisy <elbadisyimad@gmail.com>
2026-08-31 15:03:11 +0100

    Optimize core join and materialization hot paths

--- src/bt_read.cpp
+++ src/bt_read.cpp
@@ -174,6 +174,7 @@ extern "C" SEXP btread_(SEXP s_path, SEXP s_delim, SEXP s_quote, SEXP s_comment,
     opt.na_strings.clear();
     for (R_xlen_t i = 0; i < Rf_xlength(s_na); ++i)
       opt.na_strings.push_back(CHAR(STRING_ELT(s_na, i)));
+    opt.refresh_na_fast_path();
   }
 
   RowIndex index = build_row_index_mt(file->data, file->size, opt, opt.n_threads);
